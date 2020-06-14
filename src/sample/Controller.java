90ec576aec1111b5d639f8363143917b625fd094
Aidar <aid946@gmail.com>
2020-06-14 14:26:55 +0500

    v.1.1 add icon weather, run bat file

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,14 +2,21 @@ package sample;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.StreamCorruptedException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.text.Text;
+import javafx.scene.image.Image;
+import javafx.scene.text.Text;;
+import javafx.scene.image.ImageView;
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import javax.swing.*;
+
 
 public class Controller {
 
@@ -41,6 +48,9 @@ public class Controller {
     private Text pressure;
 
     @FXML
+    private ImageView icon;
+
+    @FXML
     void initialize() {
         assert city != null : "fx:id=\"city\" was not injected: check your FXML file 'sample.fxml'.";
         assert getData != null : "fx:id=\"getData\" was not injected: check your FXML file 'sample.fxml'.";
@@ -49,6 +59,7 @@ public class Controller {
         assert temp_max != null : "fx:id=\"temp_max\" was not injected: check your FXML file 'sample.fxml'.";
         assert temp_min != null : "fx:id=\"temp_min\" was not injected: check your FXML file 'sample.fxml'.";
         assert pressure != null : "fx:id=\"pressure\" was not injected: check your FXML file 'sample.fxml'.";
+        assert icon != null : "fx:id=\"icon\" was not injected: check your FXML file 'sample.fxml'.";
 
         getData.setOnAction(event -> {
             String getUserCity = city.getText().trim();
@@ -61,7 +72,15 @@ public class Controller {
                     temp_feels.setText("Ощущается: " + obj.getJSONObject("main").getDouble("feels_like") + " \u00b0" + "C");
                     temp_max.setText("Максимум: " + obj.getJSONObject("main").getDouble("temp_max") + " \u00b0" + "C");
                     temp_min.setText("Минимум: " + obj.getJSONObject("main").getDouble("temp_min") + " \u00b0" + "C");
-                    pressure.setText("Давление: " + obj.getJSONObject("main").getDouble("pressure") + " бар");
+                    pressure.setText("Давление: " + obj.getJSONObject("main").getDouble("pressure") + " Гпа");
+
+                    JSONArray a = obj.getJSONArray("weather"); //read weather array at json
+                    JSONObject aIcon = (JSONObject) a.get(0); //get icon of weather
+
+                    String weatherIcon = "http://openweathermap.org/img/wn/" + aIcon.get("icon") + "@2x.png";
+                    boolean backgroundLoading = true;
+                    Image image = new Image(weatherIcon, backgroundLoading);
+                    icon.setImage(image);
                 }
             }
         });
